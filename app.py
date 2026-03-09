9ff8d4eef6d294d3f1916c97fd0e8ac12c3351dc
t0m0h1 <thomashathawayhogg@gmail.com>
2026-03-09 11:05:13 +0000

    add flask routes and check

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+# Flask routes
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("dashboard.html")
 
 
 
-if __name__ == '__main__':
+# Run application
+
+if __name__ == "__main__":
     app.run(debug=True)
