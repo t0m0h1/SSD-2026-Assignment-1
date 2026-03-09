72a911c3a2b5f09bf6dd91d9eff41982f85a77de
t0m0h1 <thomashathawayhogg@gmail.com>
2026-03-09 12:23:18 +0000

    fix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 # app.config['SECRET_KEY'] = 'your_secret_key'
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.login_view = "login"
