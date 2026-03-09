e720ba016e58b84ee77a72504763730813e1f875
t0m0h1 <thomashathawayhogg@gmail.com>
2026-03-09 11:44:16 +0000

    add dependencies and update route

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
+
+
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
 
 
 # Flask routes
 @app.route("/")
 def home():
-    return render_template("dashboard.html")
+    return render_template("home.html")
 
 
 # Login route
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
 
-    if request.method == "POST":
+#     if request.method == "POST":
 
-        username = request.form["username"]
-        password = request.form["password"]
+#         username = request.form["username"]
+#         password = request.form["password"]
 
-        user = User.query.filter_by(username=username).first()
+#         # user = User.query.filter_by(username=username).first()
 
-        if user and check_password_hash(user.password_hash, password):
+#         if user and check_password_hash(user.password_hash, password):
 
-            login_user(user)
+#             login_user(user)
 
-            return redirect(url_for("dashboard"))
+#             return redirect(url_for("dashboard"))
 
-        flash("Invalid username or password")
+#         flash("Invalid username or password")
 
-    return render_template("login.html")
+#     return render_template("login.html")
 
 
 
